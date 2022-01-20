f59ee10cc30a5533d13cec369fd1f0b18b39b569
Akaniru Eunice <chieunice114@gmail.com>
2022-01-19 22:30:22 -0800

    created the cart schema and controller part

---
+++ api/controllers/cart.js
@@ -0,0 +1,25 @@
+
+
+const createCart = (req, res) => {
+    res.send('crated')
+}
+const updateCart = (req, res) => {
+    res.send('updated')
+}
+const deleteCart = (req, res) => {
+	res.send("deleted");
+};
+const getUserCart = (req, res) => {
+	res.send("get");
+};
+const getAllCart = (req, res) => {
+	res.send("all");
+};
+
+module.export = {
+    createCart,
+    updateCart,
+    deleteCart,
+    getUserCart,
+    getAllCart
+}
